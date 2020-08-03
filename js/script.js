930e47a0ef2ebe4febe0773048b5d6e981a91626
Rohit Chaudhari <43547779+RC2427@users.noreply.github.com>
2020-08-03 14:41:23 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 $(".nav-item a").click(function(e){
     $(".navbar-collapse").collapse('hide');
+    $(".animated-icon").toggleClass("open");
+});
+
+$(".nav-btn").click(function(e){
+    $(".animated-icon").toggleClass("open");
 });
